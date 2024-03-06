6e2cda7be0e5edcd4357e3afa110754f163983ca
Dimonch1k <leskiv-dmytro@lyc2.org>
2024-03-06 20:51:56 +0200

    first commit

---
+++ WPF_Classwork_1/WPF_Classwork_1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WPF_Classwork_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    tb1.Text = "Is clicked";
+        //}
+    }
+}
